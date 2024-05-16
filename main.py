bedf3b2f50fe22bd750196e87ef6783cbb3092c2
dkrivaa <dannykrivaa@hotmail.com>
2024-05-16 20:32:26 +0300

    main file

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ client = gspread.authorize(creds)
 
 book = client.open_by_key(sheet_id)
 
-book.sheet1.update_cell(1, 1, 'hello')
+book.sheet1.update_cell(1, 1, 'hello world')
 # values_list = book.sheet1.row_values(1)
 # print(values_list)
 

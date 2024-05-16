40b8faf89b917a888c94f516284c56239f8dfa90
dkrivaa <dannykrivaa@hotmail.com>
2024-05-16 12:02:00 +0300

    main file

--- main.py
+++ main.py
@@ -6,16 +6,12 @@ import json
 
 
 credentials_json_string = os.environ.get('credentials_json_string')
-
 credentials_json = json.loads(base64.b64decode(credentials_json_string))
-
 sheet_id = os.environ.get('sheet_id')
-
 creds = Credentials.from_service_account_info(credentials_json)
-
 client = gspread.authorize(creds)
 
-book = client.open_by_key(sheet_id)
+book = client.open_by_key('1tqBw2rPAKX4V_F0x1Utj30ZItMAzK29nsOil9lFyzrY')
 
 # values_list = book.sheet1.row_values(1)
 # print(values_list)

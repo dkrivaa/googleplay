5d03274a5e7d3a14357b8e908885b30a25c0209d
dkrivaa <dannykrivaa@hotmail.com>
2024-05-16 10:19:12 +0300

    main file

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ import os
 import json
 
 credentials_json_string = os.environ.get('credentials.json')
-credentials_json = json.loads(base64.b64decode(credentials_json_string))
-sheet_id = os.environ.get('sheet_id')
 
-creds = Credentials.from_service_account_info(credentials_json)
+print(credentials_json_string)
 
-client = gspread.authorize(creds)
-
-book = client.open_by_key(sheet_id)
-
-values_list = book.sheet1.row_values(1)
-print(values_list)
+# credentials_json = json.loads(base64.b64decode(credentials_json_string))
+# sheet_id = os.environ.get('sheet_id')
+#
+# creds = Credentials.from_service_account_info(credentials_json)
+#
+# client = gspread.authorize(creds)
+#
+# book = client.open_by_key(sheet_id)
+#
+# values_list = book.sheet1.row_values(1)
+# print(values_list)
 

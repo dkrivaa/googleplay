3ce33017c9fd53c3d3d163c3a17b29a3f4601803
dkrivaa <dannykrivaa@hotmail.com>
2024-05-16 09:53:14 +0300

    main file

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import base64
+
 import gspread
 from google.oauth2.service_account import Credentials
 import os
+import json
 
-credentials_json = os.environ.get('credentials.json')
+credentials_json_string = os.environ.get('credentials.json')
+credentials_json = json.loads(base64.b64decode(credentials_json_string))
 sheet_id = os.environ.get('sheet_id')
 
 creds = Credentials.from_service_account_info(credentials_json)

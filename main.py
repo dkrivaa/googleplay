51e1094ea2c5340be696ba9e62bd832607e09ce2
dkrivaa <dannykrivaa@hotmail.com>
2024-05-16 08:33:56 +0300

    main file

--- main.py
+++ main.py
@@ -2,5 +2,15 @@ import gspread
 from google.oauth2.service_account import Credentials
 import os
 
+credentials_json = os.environ.get('credentials.json')
+sheet_id = os.environ.get('sheet_id')
 
+creds = Credentials.from_service_account_info(credentials_json)
+
+client = gspread.authorize(creds)
+
+book = client.open_by_key(sheet_id)
+
+values_list = book.sheet1.row_values(1)
+print(values_list)
 
